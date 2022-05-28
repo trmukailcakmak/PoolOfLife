f646f8c1f578809324645e36850db979bc95d86c
Mükail Çakmak <tr.mukailcakmak@gmail.com>
2022-05-28 12:27:01 +0300

    [mikailcakmak] projeyi ilklendirdim

---
+++ src/main/java/tr/com/cakmak/pool/PoolOfLifeApplication.java
@@ -0,0 +1,13 @@
+package tr.com.cakmak.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolOfLifeApplication.class, args);
+	}
+
+}
